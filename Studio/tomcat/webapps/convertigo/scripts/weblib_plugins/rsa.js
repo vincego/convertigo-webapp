22348a1f038eddb2ce9d4a1bbcc35a2bbf26eddb
Nicolas Albert <nicolasa@convertigo.com>
2013-10-08 15:29:34 +0000

    Ref #3637 - Prevent replay of requests when the C8O encryption plugin is enabled
    Just remove the useless string revert and port the timestamp parameter to the old rsa plugins.
    
    Former-commit-id: 0cccb6394855b24091d70ebfb7ae6f99d2f669f2
    Former-commit-id: 564c3d3474a32abc575d477f3ca3218e51aa64b1
    Former-commit-id: ab98868f95fc85bf791e1fafd34c871351549174

--- Studio/tomcat/webapps/convertigo/scripts/weblib_plugins/rsa.js
+++ Studio/tomcat/webapps/convertigo/scripts/weblib_plugins/rsa.js
@@ -21,11 +21,9 @@
  */
 
 C8O._init_rsa = function (params) {
-	C8O.addHook("_call_rsa", function (data) {
-		C8O.log.trace("c8o.core: make RSA encryption");
-		return {__encoded: encrypt("ts=" + new Date().getTime() + "&" + $.param(data), C8O._define.publickey)};
+	C8O.addHook("call", function (data) {
+		C8O._obj_replace(data, {__encoded: encrypt("ts=" + new Date().getTime() + "&" + $.param(data), C8O._define.publickey)});
 	});
-	C8O._define.publickey = "";
 
 	$.ajax({
 		dataType: "text",
@@ -34,7 +32,7 @@ C8O._init_rsa = function (params) {
 			C8O._define.publickey = setKeyPair(split[0], split[1], split[2]);
 			C8O._init(params);
 		},
-		url: window.location.href.replace(new RegExp("/projects/.*"), "/rsa/publickey")
+		url: "../../rsa/publickey"
 	});
 }
 
